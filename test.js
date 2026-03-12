9cf0cfec50e7eb4894f775c5d216041a017a0240
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-03-12 16:17:38 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,33 +1,6 @@
-// --- Block 3 & 4 merged: Validate SOAPMessage and Element when using WSDL
-// Why: SOAP validation requires SOAPMessage to define version and Element to target operation.
-
-if (value.startsWith('wsdl://')) {
-
-  let soap = xpath.select('/MessageValidation/SOAPMessage', el);
-  if (soap.length === 0) {
-    compliant = false;
-
-    policy.addMessage({
-      plugin: plugin,
-      line: resource[0].lineNumber,
-      column: resource[0].columnNumber,
-      message:
-        `Missing required element "SOAPMessage" for WSDL validation in policy "${policy.getName()}".`,
-    });
-  }
-
-  let element = xpath.select('/MessageValidation/Element', el);
-  if (element.length === 0) {
-
-    policy.addMessage({
-      plugin: plugin,
-      line: resource[0].lineNumber,
-      column: resource[0].columnNumber,
-      message:
-        'Missing required "Element" in MessageValidation policy "' +
-        policy.getName() +
-        '" when using WSDL.',
-    });
-  }
-
-}
+MAIN TITLE
+API Security Controls
+ApigeeLint Security Plugins Documentation
+Green box
+Automated Security Controls for APIs
+Documentation of security requirements, Apigee policy implementations and ApigeeLint rules used to enforce API security standards within the BNP Paribas API platform.
