e766d115612da5b935d57571af24349475446f7b
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-03-17 10:22:25 +0100

    Update

--- test.js
+++ test.js
@@ -1,27 +1,26 @@
-flows.forEach((flow) => {
+if (!hasVerbCondition) {
 
-  const conditionObj = flow.getCondition();
-  const condition = conditionObj ? conditionObj.getExpression() : "";
-
-  // ✅ détecter request.verb
-  if (condition.includes('request.verb')) {
-    hasVerbCondition = true;
-  }
-
-  const steps = flow.steps || [];
-
-  steps.forEach(step => {
-
-    // ✅ detect RaiseFault (catch-all)
-    if (!condition && step.name && step.name.toLowerCase().includes('rf-')) {
-      hasCatchAllRaiseFault = true;
-    }
-
-    // ✅ detect AssignMessage
-    if (step.name && step.name.toLowerCase().includes('assignmessage')) {
-      hasAssignMessage = true;
-    }
-
+  endpoint.addMessage({
+    plugin,
+    message: 'No HTTP method control detected. Proxy does not check "request.verb".'
   });
 
-});
+} else if (!hasCatchAllRaiseFault) {
+
+  if (hasAssignMessage) {
+
+    endpoint.addMessage({
+      plugin: warningPlugin,
+      message: 'AssignMessage used without RaiseFault for method control.'
+    });
+
+  } else {
+
+    endpoint.addMessage({
+      plugin,
+      message: 'HTTP methods may fall through to backend. Missing catch-all flow with RaiseFault.'
+    });
+
+  }
+
+}
