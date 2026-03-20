9560ff5025600ea4ae3acf443292ceece38274f6
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-03-20 11:25:06 +0100

    Update

--- test.js
+++ test.js
@@ -1,41 +1,10 @@
-const preFlowSteps = xpath.select('/ProxyEndpoint/PreFlow/Request/Step', proxyEl);
+const hasGetExceptionAnnotation = endpoint => {
+  const el = endpoint.getElement();
+  const desc = xpath.select('/ProxyEndpoint/Description', el)[0];
 
-for (let i = 0; i < preFlowSteps.length; i++) {
-  const step = preFlowSteps[i];
+  if (!desc || !desc.firstChild) return false;
 
-  const conditionNode = xpath.select('Condition', step)[0];
-  const nameNode = xpath.select('Name', step)[0];
+  const text = desc.firstChild.data.toLowerCase();
 
-  const condition = (conditionNode && conditionNode.firstChild)
-    ? conditionNode.firstChild.data.trim()
-    : '';
-
-  const stepName = (nameNode && nameNode.firstChild)
-    ? nameNode.firstChild.data.trim()
-    : '';
-
-  if (conditionHasRequestVerb(condition)) {
-    foundVerbCheck = true;
-    foundPreFlowGuard = true;
-
-    if (isRaiseFaultPolicyName(endpoint, stepName)) {
-      foundRaiseFault = true;
-    }
-
-    if (conditionLooksLikeWsdlGetException(condition)) {
-      foundWsdlGetException = true;
-    }
-  }
-
-  if (isFlowCalloutPolicyName(endpoint, stepName)) {
-    foundSharedFlowDelegation = true;
-  }
-}
-
-
-
-
-const getPoliciesByType = function (endpoint, type) {
-  return endpoint.parent.getPolicies().filter(p => p.getType() === type);
+  return text.includes('apigeelit:allow-method:get:wsdl');
 };
-
