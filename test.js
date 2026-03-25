b06adeb69fc169f0e3d4ef89ee2f01ce1b681e5b
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-03-25 13:56:10 +0100

    Update

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
-const {
-  hasPolicyType,
-  getPoliciesByType,
-  getFlows,
-  getCondition,
-  getStepName
-} = require('../security-lib');
+const getFlows = endpoint =>
+  xpath.select('/ProxyEndpoint/Flows/Flow', endpoint.getElement());
+
+const getCondition = node => {
+  const condNodes = xpath.select('Condition', node);
+  if (!condNodes || condNodes.length === 0) return '';
+  const condNode = condNodes[0];
+  return (condNode && condNode.firstChild)
+    ? condNode.firstChild.data.trim()
+    : '';
+};
+
+const getStepName = node => {
+  const nameNodes = xpath.select('Name', node);
+  if (!nameNodes || nameNodes.length === 0) return '';
+  const nameNode = nameNodes[0];
+  return (nameNode && nameNode.firstChild)
+    ? nameNode.firstChild.data.trim()
+    : '';
+};
