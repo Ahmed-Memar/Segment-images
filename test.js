5fe28bbf02ca83c9fa7831919b50d8db9b9cac3b
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-04-01 11:43:07 +0200

    Update

--- test.js
+++ test.js
@@ -1,10 +1,53 @@
-invalidFlows.forEach(flow => {
-    flow.details.forEach(detail => {
-        endpoint.addMessage({
-            plugin,
-            line: detail.line,
-            column: detail.column,
-            message: `Flow "${flow.name}" error: ${detail.message}`
-        });
+const invalidFlows = findFlowsNotMatching(endpoint, (steps, flow) => {
+
+    const condition = getCondition(flow);
+    const hasVerb = requestVerbRegex.test(condition);
+
+    let hasRF = false;
+
+    steps.forEach(step => {
+        const stepName = getStepName(step);
+
+        if (isRaiseFaultPolicyUsed(endpoint, stepName)) {
+            hasRF = true;
+        }
     });
+
+    let details = [];
+
+    if (!hasVerb) {
+        details.push({
+            message: 'missing request.verb condition',
+            line: flow.lineNumber,
+            column: flow.columnNumber
+        });
+    }
+
+    if (!hasRF) {
+        details.push({
+            message: 'missing RaiseFault policy',
+            line: flow.lineNumber,
+            column: flow.columnNumber
+        });
+    }
+
+    return {
+        isValid: hasVerb && hasRF,
+        details
+    };
 });
+
+
+
+
+
+
+
+
+
+if (invalidFlows.length > 0) {
+    ...
+    return cb(null, true);
+}
+
+return cb(null, false);
