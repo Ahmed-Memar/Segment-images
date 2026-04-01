713e091b8c4f05d7db3fe3611a7361cd49484a28
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-04-01 13:59:05 +0200

    Update

--- test.js
+++ test.js
@@ -1,12 +1,6 @@
-invalidFlows.forEach(flow => {
-
-    const messages = flow.details.map(d => d.message);
-
-    endpoint.addMessage({
-        plugin,
-        line: flow.line,
-        column: flow.column,
-        message: `Flow "${flow.name}" - ${messages.join(' AND ')}`
-    });
-
-});
+if (isWsdlFlow(condition)) {
+  return {
+    isValid: true,
+    details: []
+  };
+}
