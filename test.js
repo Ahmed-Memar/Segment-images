5d60910f11b8a89b6e21fc22e1b6982cdb78cf70
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-04-01 10:23:36 +0200

    Update

--- test.js
+++ test.js
@@ -1,50 +1,40 @@
-// 1. getPreFlowRequestSteps
-
-const getPreFlowRequestSteps = endpoint =>
-    xpath.select('/ProxyEndpoint/PreFlow/Request/Step', endpoint.getElement());
+const steps = getPreFlowRequestSteps(endpoint);
 
 
-2. getFlowRequestSteps
-
-const getFlowRequestSteps = flow =>
-    xpath.select('Request/Step', flow);
-
-
-3. findFlowsNotMatching
-
-const findFlowsNotMatching = (endpoint, matcherFn) => {
-    const flows = getFlows(endpoint);
-    const invalidFlows = [];
-
-    flows.forEach(flow => {
-        const flowName = flow.getAttribute('name') || 'UnnamedFlow';
-        const flowLine = flow.lineNumber;
-        const flowColumn = flow.columnNumber;
-
-        const steps = getFlowRequestSteps(flow);
-
-        let isValid = false;
-
-        steps.forEach(step => {
-            const stepName = getStepName(step);
-            if (matcherFn(stepName, step, flow)) {
-                isValid = true;
-            }
-        });
-
-        if (!isValid) {
-            invalidFlows.push({
-                name: flowName,
-                line: flowLine,
-                column: flowColumn
-            });
-        }
-    });
-
-    return invalidFlows;
-};
+steps.forEach(step => {
+    const stepName = getStepName(step);
+    ...
+});
 
 
-    getPreFlowRequestSteps,
-    getFlowRequestSteps,
-    findFlowsNotMatching
+
+
+const invalidFlows = findFlowsNotMatching(endpoint, (stepName) => {
+    return isRequestVerbCheck(stepName) && hasRaiseFault(stepName);
+});
+
+
+
+const invalidFlows = findFlowsNotMatching(endpoint, (stepName, step, flow) => {
+
+    const condition = getCondition(flow);
+
+    const hasVerb = requestVerbRegex.test(condition);
+    const hasRF = isRaiseFaultPolicy(stepName);
+
+    return hasVerb && hasRF;
+});
+
+
+
+
+const steps = getFlowRequestSteps(flow);
+
+steps.forEach(step => {
+    const stepName = getStepName(step);
+});
+
+
+
+
+
