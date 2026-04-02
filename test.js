5a7c5b219ef522fce3c2573bfd4aa4739293b6c4
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-04-02 10:58:45 +0200

    Update

--- test.js
+++ test.js
@@ -1,6 +1,29 @@
-/**
- * Apply matcherFn to each flow and collect invalid ones
- * @param {Object} endpoint - Apigee endpoint
- * @param {(steps: Array, flow: Object) => {isValid: boolean, details: Array}} matcherFn
- * @returns {Array} invalid flows with metadata and details
- */
+return steps.some(step => {
+  const condition = getCondition(step);
+
+  if (isWsdlFlow(condition)) return false;
+
+  const stepName = getStepName(step);
+
+  return conditionHasRequestVerb(condition) &&
+         isRaiseFaultPolicyUsed(endpoint, stepName);
+});
+
+
+
+
+
+const hasRF = steps.some(step =>
+  isRaiseFaultPolicyUsed(endpoint, getStepName(step))
+);
+
+
+debug(`Inspecting proxy endpoint "${endpoint.getName()}"`);
+
+
+
+const details = [];
+
+
+
+
