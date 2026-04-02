a3453fae401a5b8cbf363ec826268ee55c6e03e1
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-04-02 14:09:31 +0200

    Update

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
 /**
  * Apply matcherFn to each flow and collect invalid ones
  *
- * @param {(steps: Array<Element>, flow: Element) => {isValid: boolean, details: Array<{message: string, line: number, column: number}>}} matcherFn
+ * @param {Function} matcherFn
  * Function used to validate a flow based on its Request steps
  *
- * @returns {Array<{name: string, line: number, column: number, details: Array}>}
- * List of invalid flows with metadata and validation details
+ * matcherFn arguments:
+ * - steps: Array
+ *   Array of Request Step elements (Element[])
+ * - flow: Element
+ *   Flow XML element
+ *
+ * matcherFn return:
+ * - isValid: boolean
+ * - details: Array
+ *   Array of {message: string, line: number, column: number}
+ *
+ * @returns {Array}
+ * Array of invalid flows:
+ * - name: string
+ * - line: number
+ * - column: number
+ * - details: Array
  */
