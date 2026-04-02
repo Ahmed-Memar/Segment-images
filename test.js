b299ee31ac99e4e9a92d524b3d7f2582cb953114
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-04-02 10:48:46 +0200

    Update

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-if (isWsdlFlow(condition)) {
-  return {
-    isValid: true,
-    details: []
-  };
-}
+/**
+ * Apply matcherFn to each flow and collect invalid ones
+ * @param {Object} endpoint - Apigee endpoint
+ * @param {(steps: Array, flow: Object) => {isValid: boolean, details: Array}} matcherFn
+ * @returns {Array} invalid flows with metadata and details
+ */
