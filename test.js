ab1fc6a7bc5f4af71bf37125fdb2dc7e59f59391
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-05-22 16:29:13 +0200

    Update

--- test.js
+++ test.js
@@ -1,11 +1,28 @@
-const xpath = require('xpath');
+const ruleId = 'EX-CS004';
+
+const debug = require('debug')('apigeelint:' + ruleId);
+
+const {
+    getFirstNode,
+    PolicyChecker
+} = require('./lib/security-lib.js');
+
+const plugin = {
+    ruleId: ruleId,
+    name: "Check SpikeArrest",
+    message: "Check if SpikeArrest is present and correctly configured",
+    fatal: false,
+    severity: 2, // 0 = off, 1 = warn, 2 = error
+    nodeType: "Bundle",
+    enabled: true,
+};
 
 const configCheckCallback = function(policy) {
     let compliant = true;
 
-    let item = xpath.select('/SpikeArrest/Rate', policy.getElement());
+    let rate = getFirstNode('/SpikeArrest/Rate', policy.getElement());
 
-    if (item.length == 0) {
+    if (!rate) {
         compliant = false;
         policy.addMessage({
             plugin: plugin,
@@ -16,13 +33,26 @@ const configCheckCallback = function(policy) {
     }
 
     return compliant;
-}
+};
 
+const onProxyEndpoint = function(endpoint, cb) {
+    debug(`Inspecting proxy endpoint "${endpoint.getName()}"`);
 
+    let checker = new PolicyChecker(
+        plugin,
+        'SpikeArrest',
+        debug,
+        configCheckCallback
+    );
 
-let checker = new SecurityLib.PolicyChecker(
+    let hasIssue = checker.check(endpoint);
+
+    if (typeof cb == 'function') {
+        cb(null, hasIssue);
+    }
+};
+
+module.exports = {
     plugin,
-    'SpikeArrest',
-    debug,
-    configCheckCallback
-);
+    onProxyEndpoint,
+};
