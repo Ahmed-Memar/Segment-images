1d17cf98bc43ba88bfef30adf605ea65a55f9b26
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-05-22 11:50:33 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,24 +1,3 @@
-const invalidFlows = endpoint.getFlows().filter(flow => {
-    const condition = flow.getCondition();
-    const expression = condition ? condition.getExpression() || '' : '';
-
-    if (!BODY_METHOD_REGEX.test(expression)) {
-        return false; // ignore GET etc
-    }
-
-    let hasJSON = preFlowHasJSON;
-    let hasXML = preFlowHasXML;
-
-    const steps = getFlowRequestSteps(flow.getElement()) || [];
-
-    steps.forEach(step => {
-        if (stepUsesJSON(endpoint, step)) {
-            hasJSON = true;
-        }
-        if (stepUsesXML(endpoint, step)) {
-            hasXML = true;
-        }
-    });
-
-    return !hasJSON && !hasXML;
-});
+const expression = flow.getChild('Condition')
+  ? flow.getChild('Condition').getText()
+  : '';
