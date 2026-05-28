0625a7edd38ed8036b977031fa1a52845cb411c7
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-05-28 15:06:33 +0200

    Update

--- test.js
+++ test.js
@@ -1,8 +1,23 @@
-endpoint.addMessage({
-    plugin: usedPlugin,
-    line: r.step.lineNumber,
-    column: r.step.columnNumber,
-    message:
-        `PreFlow is not compliant: ` +
-        `Step "${getStepName(r.step)}" ${r.analysis.message}`
-});
+details: [{
+    stepName: getStepName(step),
+    line: step.lineNumber,
+    column: step.columnNumber,
+    message: 'uses JSON but no JSONThreatProtection policy is applied'
+}]
+
+details: [{
+    stepName: getStepName(step),
+    line: step.lineNumber,
+    column: step.columnNumber,
+    message: 'uses JSON from unknown source but no JSONThreatProtection policy is applied'
+}]
+
+line: r.line,
+column: r.column,
+message:
+    `PreFlow is not compliant: ` +
+    `Step "${r.stepName}" ${r.message}`
+
+const messages = flow.details.map(d =>
+    `Step "${d.stepName}" ${d.message}`
+);
