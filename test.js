261b46ebab97b8929f2e3a4319481e13e7a983dd
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-05-28 14:46:12 +0200

    Update

--- test.js
+++ test.js
@@ -1,23 +1,8 @@
-return {
-    usesJson: true,
-    severity: 'error',
-    message: 'uses JSON but no JSONThreatProtection policy is applied'
-};
-
-return {
-    usesJson: true,
-    severity: 'warning',
-    message: 'uses JSON from unknown source but no JSONThreatProtection policy is applied'
-};
-
-message:
-    `Flow "${flow.name}" is not compliant: ` +
-    `Step "${getStepName(detail.step)}" ${detail.message}`
-
-details: jsonSteps.map(r => ({
-    step: r.step,
+endpoint.addMessage({
+    plugin: usedPlugin,
     line: r.step.lineNumber,
     column: r.step.columnNumber,
-    severity: r.analysis.severity,
-    message: r.analysis.message
-}))
+    message:
+        `PreFlow is not compliant: ` +
+        `Step "${getStepName(r.step)}" ${r.analysis.message}`
+});
