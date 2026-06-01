dd0df2e8c7c6868d2157aa3bb635eacdda2a6d1a
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-06-01 14:43:54 +0200

    Update

--- test.js
+++ test.js
@@ -1,40 +1,8 @@
-<ServiceCallout name="SC-External">
-    <Response>externalResponse</Response>
-    <HTTPTargetConnection>
-        <URL>https://api.thirdparty.com/test</URL>
-    </HTTPTargetConnection>
-</ServiceCallout>
-
-
-
-<ExtractVariables name="EV-ExtractJSON">
-    <JSONPayload>
-        <Variable name="id">
-            <JSONPath>$.id</JSONPath>
-        </Variable>
-    </JSONPayload>
-    <Source>externalResponse</Source>
-</ExtractVariables>
-
-
-
-<Flow name="Flow1">
-    <Request>
-        <Step>
-            <Name>SC-External</Name>
-        </Step>
-        <Step>
-            <Name>EV-ExtractJSON</Name>
-        </Step>
-    </Request>
-</Flow>
-
-
-console.log('Policies:', policies.length);
-
-policies.forEach(p =>
-    console.log(
-        p.getName ? p.getName() : p.name,
-        p.getType ? p.getType() : p.type
-    )
+const loadBalancerServerNode = getFirstNode(
+    '/ServiceCallout/HTTPTargetConnection/LoadBalancer/Server',
+    element
 );
+
+if (loadBalancerServerNode) {
+    trust = 'internal';
+}
