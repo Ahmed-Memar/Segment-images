0d2123334242944b6215f40c546f575ae256e950
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-06-05 11:15:42 +0200

    Update

--- test.js
+++ test.js
@@ -1,6 +1,22 @@
+/**
+ * Returns true if the Step uses JSON processing.
+ *
+ * @param {Object} endpoint
+ * @param {Node} step
+ *
+ * @returns {boolean}
+ */
 const stepHasJSON = (endpoint, step) =>
     stepUsesJSON(endpoint, step, {}).usesJson === true;
 
+/**
+ * Returns true if the Step uses XML processing.
+ *
+ * @param {Object} endpoint
+ * @param {Node} step
+ *
+ * @returns {boolean}
+ */
 const stepHasXML = (endpoint, step) =>
     stepUsesXML(endpoint, step, {}).usesXML === true;
 
