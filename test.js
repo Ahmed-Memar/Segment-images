4d333c4dc1bfa9eedf7d354596c95a85dc580173
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-06-05 15:48:32 +0200

    Update

--- test.js
+++ test.js
@@ -1,15 +1,43 @@
-/**
- * Returns true if the Step uses XML processing that requires XMLThreatProtection.
- *
- * @param {Object} endpoint
- * @param {Node} step
- * @param {Object<string, string>} registry
- *
- * @returns {boolean}
- */
-const stepRequiresXMLProtection = function (endpoint, step, registry) {
-    const analysis = stepUsesXML(endpoint, step, registry);
+const expression = getCondition(flow) || '';
 
-    return analysis.usesXML === true &&
-        analysis.severity !== 'ignore';
-};
+// Only body request flows are validated by default
+if (!BODY_METHOD_REGEX.test(expression)) {
+    return {
+        isValid: true,
+        details: []
+    };
+}
+
+const steps = getFlowRequestSteps(flow) || [];
+
+// If the flow clearly processes XML, let XMLThreatProtection plugin handle it
+const xmlSteps = steps.filter(step =>
+    stepRequiresXMLProtection(
+        endpoint,
+        step,
+        variableRegistry
+    )
+);
+
+if (xmlSteps.length > 0) {
+    return {
+        isValid: true,
+        details: []
+    };
+}
+
+
+
+if (jtpPolicies.length === 0) {
+    return {
+        isValid: false,
+        details: [{
+            line: flow.lineNumber,
+            column: flow.columnNumber,
+            severity: 'error',
+            message:
+                'may process a JSON request body by default ' +
+                'but no JSONThreatProtection policy is applied'
+        }]
+    };
+}
