0d54bd9f6086fd4028a39068eee606ac20ec9939
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-06-12 19:13:16 +0200

    Update

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-console.log('algorithmNode=', algorithmNode);
-console.log('policyLine=', policyLine);
-console.log('policyColumn=', policyColumn);
+if (!algorithmValue) {
+    console.log('BEFORE ERROR PUSH');
+
+    errors.push({
+        line: policyLine,
+        column: policyColumn,
+        message: `VerifyJWT policy "${policyName}" must define <Algorithm>.`
+    });
+
+    console.log('AFTER ERROR PUSH');
+}
