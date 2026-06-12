e99092a3ee3b56ce7d8435394e258ccb9719eaf4
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-06-12 18:25:42 +0200

    Update

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
-// HS* requires <SecretKey>, while RS*/PS*/ES* require <PublicKey>.
-// Mixing both families would require incompatible key types.
-if (usesHS && usesAsymmetric) {
-    errors.push({
-        line: getNodeLine(algorithmNode, policyLine),
-        column: getNodeColumn(algorithmNode, policyColumn),
-        message: `VerifyJWT policy "${policyName}" mixes symmetric (HS*) and asymmetric (RS*/PS*/ES*) algorithms, which is not supported.`
-    });
-}
+console.log('policy=', policyName);
+console.log('returning=', {
+    policy,
+    isValid: errors.length === 0
+});
+
+
