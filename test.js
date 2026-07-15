eff1b60b8d0b1c7c733bb618f198827f287d1b73
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-07-15 11:05:24 +0200

    Update

--- test.js
+++ test.js
@@ -1,19 +1,26 @@
 build_scanner_image:
   stage: build
 
-  extends: .buildah:build
+  image:
+    name: image-registry.openshift-image-registry.svc:5000/ns001b004551/buildah:0.2-beta
 
   variables:
-    IMAGE_NAME: apigeelint-security-plugins
-    BUILD_CONTEXT: .
+    BUILDAH_ISOLATION: chroot
+    STORAGE_DRIVER: vfs
 
+  script:
+    - buildah --version
 
+    - |
+      buildah bud \
+        --file Dockerfile \
+        --tag apigeelint-security-plugins:$CI_COMMIT_SHORT_SHA \
+        --build-arg CI_REGISTRY="$CI_REGISTRY" \
+        --build-arg ARTIFACTORY_PROD_USER="$ARTIFACTORY_PROD_USER" \
+        --build-arg ARTIFACTORY_PROD_PASSWORD="$ARTIFACTORY_PROD_PASSWORD" \
+        .
 
+    - buildah images
 
-include:
-  - project: 'Production-mutualisee/IPS/IDO/gitlab-cicd/pipelines'
-    file: '.gitlab-ci.yml'
-
-  - project: 'market-place/a100133/ci-cd/gitlab-ci-templates'
-    ref: '0.16.4'
-    file: 'buildah/buildah.yml'
+  rules:
+    - if: '$CI_COMMIT_BRANCH == "docker-runtime"'
