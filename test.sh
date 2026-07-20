d44151ccc46ba86e21c7bd1bbfce2232e15b46f6
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-07-20 15:56:58 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,1 @@
-node_modules/
-.apigeelint-results/
-apigeelint-results.json
-apigeelint-stderr.log
-gl-sast-report.json
-*.tgz
-package-consumer/
-
-
-
-
-rm -f apigeelint-results.json apigeelint-stderr.log gl-sast-report.json
+.
