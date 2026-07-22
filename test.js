ae504bc21f092a6c689b73313e373f44cae60919
Memar99 <159933095+Ahmed-Memar@users.noreply.github.com>
2026-07-22 12:12:34 +0200

    Update

--- test.js
+++ test.js
@@ -1,2 +1,17 @@
+cd "$HOME/apigeelint-offline-test"
+
+rm -rf node_modules package-lock.json
+rm -rf "$HOME/apigeelint-empty-cache"
+
+npm install --offline \
+  --cache "$HOME/apigeelint-empty-cache" \
+  --no-save \
+  "$HOME/apigeelint-security-plugins/apigeelint-security-plugins-0.2.1.tgz"
+
+
+
+
+
+
 ./node_modules/.bin/apigeelint-security scan \
   "$HOME/consumer project/mybundles"
